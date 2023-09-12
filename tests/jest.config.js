ac778af780aa5a5517621beff10d489d350dcdac
GuilaneDen <83951892+GuilaneDen@users.noreply.github.com>
2023-09-12 12:19:55 +0200

    test(withdrawSelfApeCoin): Implemented the withdrawSelfApeCoin test

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -26,9 +26,10 @@ module.exports = {
 
     // The glob patterns Jest uses to detect test files
     testMatch: [
-        '**/__tests__/**/*.[jt]s?(x)',
-        '**/?(*.)+(spec|test).[tj]s?(x)',
-        '**/?(*.)+(ispec|test).[tj]s?(x)',
+        // '**/__tests__/**/*.[jt]s?(x)',
+        // '**/?(*.)+(spec|test).[tj]s?(x)',
+        // '**/?(*.)+(ispec|test).[tj]s?(x)',
+        '**/ethereum_withdraw_self_apecoin.test.js',
     ],
 
     // Stop immediatly when a test fails
