2156ec95242208161fdb9a1f4588ccef698ad785
GuilaneDen <83951892+GuilaneDen@users.noreply.github.com>
2023-09-19 10:53:10 +0200

    test(redeem): Implemented the redeem test

--- tests/src/avalanche/avalanche_redeem.test.js
+++ tests/src/avalanche/avalanche_redeem.test.js
@@ -2,26 +2,28 @@ import { processTest, populateTransaction } from "../test.fixture";
 
 const contractName = "TransparentUpgradeableProxy";  // <= Name of the smart contract
 
-const testLabel = "ethereum_morpho_withdraw"; // <= Name of the test
-const testNetwork = "ethereum";
+const testLabel = "avalanche_redeem"; // <= Name of the test
+const testDirSuffix = "avalanche_redeem"; // <= directory to compare device snapshots to
+const testNetwork = "avalanche";
 const signedPlugin = false;
 
-const contractAddr = "0x777777c9898d384f785ee44acfe945efdff5f3e0";   // <= Address of the smart contract
-const chainID = 1;
+const contractAddr = "0x2b2c81e08f1af8835a78bb2a90ae924ace0ea4be";   // <= Address of the smart contract
+const chainID = 43114;
+
 
 const transactions = [
     {
-        // From : https://etherscan.io/tx/0xdfdb427651807ed9ef26f15c3d0d3faed76d4529fdefc18309295ecac165f8c3
-        inputData: '0xf3fef3a3000000000000000000000000030ba81f1c18d280636f32af80b9aad02cf0854e0000000000000000000000000000000000000000000000049b9ca9a6943400000000000000000000000000000000000000000000000000000000018a8dba6d6b000000000000000000000000000000000000000000000000000000000000da44',
-        nanoStep: 4,
-        nanoSPtep: 4,
-        nanoXtep: 4,
+        // From : https://snowtrace.io/tx/0xff1f51fae1bdb50dd47ce41bf0e740612c6480d62842c2ea1494593bbfd59540
+        inputData: '0xbe040fb0',
+        nanoStep: 7,
+        nanoSPtep: 5,
+        nanoXtep: 5,
         index: 1,
     },
     {
-        // From : https://etherscan.io/tx/0x444f2d0401e982b452dc1259a98cc982929855f59dad57a48f6f8096c37dc841
-        inputData: '0x69328dec0000000000000000000000009ff58f4ffb29fa2266ab25e75e2a8b3503311656000000000000000000000000000000000000000000000000000000000000271000000000000000000000000025ebad8a8d3246c127f5e61984651398558ec4ee',
-        nanoStep: 7,
+        // From : https://snowtrace.io/tx/0xe930e5431db191d40cdc3db840a21779184ae9175889aafab7fbd4761827f067
+        inputData: '0xdb006a750000000000000000000000000000000000000000000000000000000000000000',
+        nanoStep: 5,
         nanoSPtep: 5,
         nanoXtep: 5,
         index: 2,
