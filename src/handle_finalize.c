8d0dd53ab8fe305770ac882a18e1c98dcab0d7f2
Z4karia <92750334+Z4karia@users.noreply.github.com>
2024-04-05 16:47:44 +0000

    fix potential null pointer dereferences

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -7,18 +7,20 @@
 static bool set_ticker_deposit_for_mapped_token(plugin_parameters_t *context,
                                                 ethPluginFinalize_t *msg) {
     for (size_t i = 0; i < NUM_SUPPORTED_SMART_CONTRACT; i++) {
-        if (!memcmp(msg->pluginSharedRO->txContent->destination,
-                    STAKEKIT_SUPPORTED_YEARN_VAULT[i].smart_contract,
-                    ADDRESS_LENGTH)) {
-            char ticker[TICKER_LEN];
-            strlcpy(ticker,
-                    (char *) STAKEKIT_SUPPORTED_YEARN_VAULT[i].token_symbol_deposit,
-                    sizeof(ticker));
-            strlcat(ticker, " ", sizeof(ticker));
-            strlcpy(context->ticker_sent, (char *) ticker, sizeof(context->ticker_sent));
-            context->decimals_sent = STAKEKIT_SUPPORTED_YEARN_VAULT[i].decimals_sent;
-            context->tokens_found |= TOKEN_SENT_FOUND;
-            return true;
+        if (msg != NULL && msg->pluginSharedRO != NULL && msg->pluginSharedRO->txContent != NULL) {
+            if (!memcmp(msg->pluginSharedRO->txContent->destination,
+                        STAKEKIT_SUPPORTED_YEARN_VAULT[i].smart_contract,
+                        ADDRESS_LENGTH)) {
+                char ticker[TICKER_LEN];
+                strlcpy(ticker,
+                        (char *) STAKEKIT_SUPPORTED_YEARN_VAULT[i].token_symbol_deposit,
+                        sizeof(ticker));
+                strlcat(ticker, " ", sizeof(ticker));
+                strlcpy(context->ticker_sent, (char *) ticker, sizeof(context->ticker_sent));
+                context->decimals_sent = STAKEKIT_SUPPORTED_YEARN_VAULT[i].decimals_sent;
+                context->tokens_found |= TOKEN_SENT_FOUND;
+                return true;
+            }
         }
     }
     return false;
@@ -30,24 +32,26 @@ static bool set_ticker_deposit_for_mapped_token(plugin_parameters_t *context,
 static bool set_ticker_withdraw_for_mapped_token(plugin_parameters_t *context,
                                                  ethPluginFinalize_t *msg) {
     for (size_t i = 0; i < NUM_SUPPORTED_SMART_CONTRACT; i++) {
-        if (!memcmp(msg->pluginSharedRO->txContent->destination,
-                    STAKEKIT_SUPPORTED_YEARN_VAULT[i].smart_contract,
-                    ADDRESS_LENGTH)) {
-            char ticker[TICKER_LEN];
-            strlcpy(ticker,
-                    (char *) STAKEKIT_SUPPORTED_YEARN_VAULT[i].token_symbol_withdraw,
-                    sizeof(ticker));
-            strlcat(ticker, " ", sizeof(ticker));
-            strlcpy(context->ticker_sent, (char *) ticker, sizeof(context->ticker_sent));
-            context->decimals_sent = STAKEKIT_SUPPORTED_YEARN_VAULT[i].decimals_sent;
-            context->tokens_found |= TOKEN_SENT_FOUND;
-            return true;
+        if (msg != NULL && msg->pluginSharedRO != NULL && msg->pluginSharedRO->txContent != NULL) {
+            if (!memcmp(msg->pluginSharedRO->txContent->destination,
+                        STAKEKIT_SUPPORTED_YEARN_VAULT[i].smart_contract,
+                        ADDRESS_LENGTH)) {
+                char ticker[TICKER_LEN];
+                strlcpy(ticker,
+                        (char *) STAKEKIT_SUPPORTED_YEARN_VAULT[i].token_symbol_withdraw,
+                        sizeof(ticker));
+                strlcat(ticker, " ", sizeof(ticker));
+                strlcpy(context->ticker_sent, (char *) ticker, sizeof(context->ticker_sent));
+                context->decimals_sent = STAKEKIT_SUPPORTED_YEARN_VAULT[i].decimals_sent;
+                context->tokens_found |= TOKEN_SENT_FOUND;
+                return true;
+            }
         }
     }
     return false;
 }
 
-void handle_finalize(void *parameters) {
+void handle_finalize(ethPluginFinalize_t *msg) {
     ethPluginFinalize_t *msg = (ethPluginFinalize_t *) parameters;
     plugin_parameters_t *context = (plugin_parameters_t *) msg->pluginContext;
 
