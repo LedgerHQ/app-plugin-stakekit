5e0f01d7938fa321d10a06085e44d92a9460f9b2
keiff3r <keiffer@blooo.io>
2025-04-07 18:29:52 +0200

    test: add stax and flex to eth_staked_usde_v2_deposit

--- tests/src/ethereum/ethereum_staked_usde_v2_deposit.test.js
+++ tests/src/ethereum/ethereum_staked_usde_v2_deposit.test.js
@@ -31,6 +31,14 @@ const devices = [
         name: "nanosp",
         label: "Nano S+",
         steps: 5, // <= Define the number of steps for this test case and this device
+    },
+    {
+        name: "stax",
+        label: "Stax",
+    },
+    {
+        name: "flex",
+        label: "Flex",
     }
 ];
 
