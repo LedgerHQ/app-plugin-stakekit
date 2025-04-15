8fe39762c16a840b34ed6a2809c33b611e761d90
keiff3r <keiffer@blooo.io>
2025-04-15 14:41:25 +0200

    feat: display the right ticker for stk-usde mint and generate golden snapshots
    LDG-674

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -72,8 +72,11 @@ void handle_finalize(ethPluginFinalize_t *msg) {
                 break;
             case STAKED_USDE_V2_MINT:
                 msg->numScreens = 2;
-                strlcpy(context->ticker_sent, STAKED_USDE_V2_TICKER, sizeof(context->ticker_sent));
-                msg->result = ETH_PLUGIN_RESULT_OK;
+                if (set_ticker_deposit_for_mapped_token(context, msg)) {
+                    msg->result = ETH_PLUGIN_RESULT_OK;
+                } else {
+                    msg->result = ETH_PLUGIN_RESULT_ERROR;
+                }
                 break;
             case COMET_CLAIM:
             case CLAIM:
