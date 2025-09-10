200fb3d735316aa71dddba1ee752c1c0f16ceb4d
Francois Beutin <francois.beutin@ledger.fr>
2025-09-10 11:46:12 +0200

    Add support for APEX_P

--- tests/src/test.fixture.js
+++ tests/src/test.fixture.js
@@ -22,13 +22,11 @@ let simOptions = {
 
 const Resolve = require('path').resolve;
 
-const APP_PATH_NANOS = Resolve('elfs/ethereum_nanos.elf');
 const APP_PATH_NANOX = Resolve('elfs/ethereum_nanox.elf');
 const APP_PATH_NANOSP = Resolve('elfs/ethereum_nanosp.elf');
 const APP_PATH_STAX = Resolve('elfs/ethereum_stax.elf');
 const APP_PATH_FLEX = Resolve('elfs/ethereum_flex.elf');
 
-const PLUGIN_LIB_NANOS = { 'StakeKit': Resolve('elfs/plugin_nanos.elf') };
 const PLUGIN_LIB_NANOX = { 'StakeKit': Resolve('elfs/plugin_nanox.elf') };
 const PLUGIN_LIB_NANOSP = { 'StakeKit': Resolve('elfs/plugin_nanosp.elf') };
 const PLUGIN_LIB_STAX = { 'StakeKit': Resolve('elfs/plugin_stax.elf') };
@@ -100,7 +98,6 @@ function zemu(device, func, testNetwork, signed = false) {
     let current_model;
 
     const models = [
-      { dev: { name: 'nanos', prefix: 'S', path: APP_PATH_NANOS }, plugin: PLUGIN_LIB_NANOS },
       { dev: { name: 'nanox', prefix: 'X', path: APP_PATH_NANOX }, plugin: PLUGIN_LIB_NANOX },
       { dev: { name: 'nanosp', prefix: 'SP', path: APP_PATH_NANOSP }, plugin: PLUGIN_LIB_NANOSP },
       { dev: { name: 'stax', prefix: 'ST', path: APP_PATH_STAX }, plugin: PLUGIN_LIB_STAX },
